f8baf166181df82486d0f13d00790c52541b0e99
Ariel Pchara <ariel@pchara.com>
2019-07-29 13:56:57 -0300

    coverall test

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,16 @@
 module.exports = {
-    "roots": [
-        "<rootDir>/src"
-    ],
-    "transform": {
-        "^.+\\.tsx?$": "ts-jest"
-    },
-    "collectCoverage": true,
-    "coverageDirectory": "coverage",
-    "coverageThreshold": {
-        "global": {
-            "branches": 80,
-            "functions": 80,
-            "lines": 80,
-            "statements": -10
-        }
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  // "collectCoverage": true,
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
     }
+  }
 }
