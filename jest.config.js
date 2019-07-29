332e290511ed6d5423249da874e6eea652872d71
Ariel Pchara <ariel@pchara.com>
2019-07-29 16:13:36 -0300

    jest

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,5 @@ module.exports = {
   roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  },
-  // "collectCoverage": true,
-  coverageDirectory: 'coverage',
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: -10
-    }
   }
 }
